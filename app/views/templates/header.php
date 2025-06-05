1e546715b4d442035c52f8635ef59a764af98a16
gema_handika <gemahandika88@gmail.com>
2025-06-05 17:44:46 +0700

    update

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,7 +1,5 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+
 $namaUser = $_SESSION['user']['name'] ?? 'NONAME';
 $UserID = $_SESSION['user']['username'] ?? 'NONAME';
 ?>
