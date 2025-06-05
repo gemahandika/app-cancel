dfba4cc20e8adc41c82607cc1948100362f0e640
gema_handika <gemahandika88@gmail.com>
2025-06-06 00:10:48 +0700

    update

--- app/controllers/Auth.php
+++ app/controllers/Auth.php
@@ -45,7 +45,6 @@ class Auth extends Controller
 
     public function logout()
     {
-        session_start();
         session_unset();  // Menghapus semua variabel session
         session_destroy(); // Menghancurkan session
         header('Location: ' . BASEURL . '/auth');
