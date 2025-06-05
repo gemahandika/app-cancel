4a87059edb0becabb70d51110c8f62e540e86478
gema_handika <gemahandika88@gmail.com>
2025-06-05 16:42:48 +0700

    update

--- app/core/App.php
+++ app/core/App.php
@@ -10,13 +10,13 @@ class App
     {
         $url = $this->parseURL();
         //controller
-        if (isset($url[0]) && file_exists('../app/controllers/' . $url[0] . '.php')) {
+        if (isset($url[0]) && file_exists('app/controllers/' . $url[0] . '.php')) {
             $this->controller = $url[0];
             unset($url[0]);
         }
 
 
-        require_once __DIR__ . '/../controllers/' . $this->controller . '.php';
+        require_once 'app/controllers/' . $this->controller . '.php';
         $this->controller = new $this->controller;
 
         // method
