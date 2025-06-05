cf2963db21978f5972d863f204f5f588cc7103ec
gema_handika <gemahandika88@gmail.com>
2025-06-06 01:00:21 +0700

    updaaaet

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -8,10 +8,10 @@ class Home extends Controller
 {
     public function __construct()
     {
-        if (!isset($_SESSION['user'])) {
-            header('Location: ' . BASEURL . '/auth');
-            exit;
-        }
+        // if (!isset($_SESSION['user'])) {
+        //     header('Location: ' . BASEURL . '/auth');
+        //     exit;
+        // }
     }
 
     public function index()
