243071d6292ea32195e68d98c2a523b729d98333
gema_handika <gemahandika88@gmail.com>
2025-06-05 16:11:00 +0700

    update

--- app/core/App.php
+++ app/core/App.php
@@ -16,7 +16,7 @@ class App
         }
 
 
-        require_once '../app/controllers/' . $this->controller . '.php';
+        require_once __DIR__ . '/../controllers/' . $this->controller . '.php';
         $this->controller = new $this->controller;
 
         // method
