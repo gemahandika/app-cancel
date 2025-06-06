0d543826082831dc36f1c2559436806aa191827f
gema_handika <gemahandika88@gmail.com>
2025-06-07 03:12:16 +0700

    update

--- app/core/App.php
+++ app/core/App.php
@@ -1,4 +1,3 @@
-
 <?php
 class App
 {
@@ -8,31 +7,38 @@ class App
 
     public function __construct()
     {
+        // Parsing URL
         $url = $this->parseURL();
-        var_dump($url);
-        exit; // Debug: cek hasil parsing
+        var_dump($url);  // Debug: cek hasil parsing
+        exit;
 
+        // Menentukan controller
         if (isset($url[0]) && file_exists('app/controllers/' . ucfirst($url[0]) . '.php')) {
-            $this->controller = ucfirst($url[0]);
-            unset($url[0]);
+            $this->controller = ucfirst($url[0]); // Mengambil controller dari URL
+            unset($url[0]);  // Menghapus controller dari URL setelah diproses
         }
 
+        // Memanggil file controller
         require_once 'app/controllers/' . $this->controller . '.php';
         $this->controller = new $this->controller;
 
+        // Menentukan method
         if (isset($url[1]) && method_exists($this->controller, $url[1])) {
-            $this->method = $url[1];
-            unset($url[1]);
+            $this->method = $url[1]; // Mengambil method dari URL
+            unset($url[1]);  // Menghapus method dari URL setelah diproses
         }
 
+        // Mengambil parameter (sisa URL setelah controller dan method)
         $this->params = $url ? array_values($url) : [];
 
+        // Memanggil method pada controller dengan parameter
         call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     public function parseURL()
     {
         if (isset($_GET['url'])) {
+            // Memecah URL berdasarkan '/' dan membersihkan karakter yang tidak perlu
             return explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
         }
         return [];
