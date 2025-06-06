c97531e9b5909ab711954b8758c20a2cd577dad3
gema_handika <gemahandika88@gmail.com>
2025-06-07 00:07:07 +0700

    update

--- app/init.php
+++ app/init.php
@@ -1,7 +1,7 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+ini_set('session.cookie_domain', '.jnemedan.com');
+if (!session_id()) session_start();
+
 require_once 'core/App.php';
 require_once 'core/Controller.php';
 require_once 'core/Database.php';
