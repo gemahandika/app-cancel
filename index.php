02ee331cbc82933804061529044e67900109edc1
gema_handika <gemahandika88@gmail.com>
2025-06-06 23:51:31 +0700

    update

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if (!session_id()) session_start();
+
 require_once 'app/init.php';
 
 $app = new App;
