4c81a73ad6b4126da9a76f53eb3156e2f55dec64
gema_handika <gemahandika88@gmail.com>
2025-06-07 02:52:32 +0700

    update

--- app/core/App.php
+++ app/core/App.php
@@ -1,5 +1,5 @@
 
-<?
+<?php
 class App
 {
     protected $controller = 'Home';
@@ -10,23 +10,21 @@ class App
     {
         $url = $this->parseURL();
         var_dump($url);
-        exit;
-        // Controller
-        if (isset($url[0]) && file_exists('../app/controllers/' . ucfirst($url[0]) . '.php')) {
+        exit; // Debug: cek hasil parsing
+
+        if (isset($url[0]) && file_exists('app/controllers/' . ucfirst($url[0]) . '.php')) {
             $this->controller = ucfirst($url[0]);
             unset($url[0]);
         }
 
-        require_once '../app/controllers/' . $this->controller . '.php';
+        require_once 'app/controllers/' . $this->controller . '.php';
         $this->controller = new $this->controller;
 
-        // Method
         if (isset($url[1]) && method_exists($this->controller, $url[1])) {
             $this->method = $url[1];
             unset($url[1]);
         }
 
-        // Params
         $this->params = $url ? array_values($url) : [];
 
         call_user_func_array([$this->controller, $this->method], $this->params);
