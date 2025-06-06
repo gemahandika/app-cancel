028564f21936a41c22ea405869db6d357203d051
gema_handika <gemahandika88@gmail.com>
2025-06-07 03:21:13 +0700

    update

--- app/core/App.php
+++ app/core/App.php
@@ -8,31 +8,35 @@ class App
 
     public function __construct()
     {
+        // Parse URL
         $url = $this->parseURL();
-        var_dump($url); // Debug: cek hasil parsing
-        exit; // Debug exit point
+        var_dump($url); // Debugging: Lihat apakah URL terurai dengan benar
+        exit; // Debugging exit point
 
-        // Controller
+        // Cek apakah controller ada di direktori app/controllers
         if (isset($url[0]) && file_exists('app/controllers/' . ucfirst($url[0]) . '.php')) {
-            $this->controller = ucfirst($url[0]);
-            unset($url[0]);
+            $this->controller = ucfirst($url[0]); // Controller yang sesuai
+            unset($url[0]); // Hapus controller dari URL
         }
 
+        // Include controller
         require_once 'app/controllers/' . $this->controller . '.php';
         $this->controller = new $this->controller;
 
-        // Method
+        // Cek apakah method ada di controller
         if (isset($url[1]) && method_exists($this->controller, $url[1])) {
-            $this->method = $url[1];
-            unset($url[1]);
+            $this->method = $url[1]; // Method yang sesuai
+            unset($url[1]); // Hapus method dari URL
         }
 
-        // Params
+        // Param yang akan diteruskan ke method
         $this->params = $url ? array_values($url) : [];
 
+        // Panggil method dengan params yang sesuai
         call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
+    // Fungsi untuk memparse URL
     public function parseURL()
     {
         if (isset($_GET['url'])) {
