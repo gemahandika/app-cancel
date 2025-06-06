29ef5eefa47d766cb5ce6869c5e3d29e867e3842
gema_handika <gemahandika88@gmail.com>
2025-06-07 02:20:54 +0700

    update

--- app/core/App.php
+++ app/core/App.php
@@ -8,8 +8,8 @@ class App
     public function __construct()
     {
         $url = $this->parseURL();
-
-        // Controller
+        var_dump($url); // Cek URL yang diparse
+        // controller
         if (isset($url[0]) && file_exists('app/controllers/' . $url[0] . '.php')) {
             $this->controller = $url[0];
             unset($url[0]);
@@ -18,7 +18,7 @@ class App
         require_once 'app/controllers/' . $this->controller . '.php';
         $this->controller = new $this->controller;
 
-        // Method
+        // method
         if (isset($url[1])) {
             if (method_exists($this->controller, $url[1])) {
                 $this->method = $url[1];
@@ -26,12 +26,12 @@ class App
             }
         }
 
-        // Params
+        // params
         if (!empty($url)) {
             $this->params = array_values($url);
         }
 
-        // Jalankan controller dan method serta kirimkan params jika ada
+        // jalankan controller dan method serta kirimkan params jika ada
         call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
