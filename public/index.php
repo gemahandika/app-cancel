22edec28251710a9da9d156fa0eb4a71d409979d
gema_handika <gemahandika88@gmail.com>
2025-06-07 11:35:00 +0700

    update

--- public/index.php
+++ public/index.php
@@ -1,6 +1,14 @@
 <?php
 ini_set('session.save_path', '/tmp');
+session_set_cookie_params([
+    'lifetime' => 0,
+    'path' => '/',
+    'domain' => 'resicancel.jnemedan.com',
+    'secure' => false, // ubah ke true jika HTTPS aktif
+    'httponly' => true,
+]);
 session_start();
+
 
 
 require_once '../config/config.php';
