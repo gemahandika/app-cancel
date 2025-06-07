a17bef903f6ee63c00e5f6e5c2744d83352b5bc2
gema_handika <gemahandika88@gmail.com>
2025-06-07 11:26:53 +0700

    update

--- app/controllers/Dashboard.php
+++ app/controllers/Dashboard.php
@@ -5,8 +5,7 @@ class Dashboard extends Controller
     public function index()
     {
         session_start();
-        var_dump($_SESSION); // cek apakah session terbaca
-        exit;
+
         if (!isset($_SESSION['user'])) {
             header('Location: ' . BASEURL . '/login');
             exit;
