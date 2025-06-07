09fda79b0ab733ceaabb0add7c1d9379a38295a1
gema_handika <gemahandika88@gmail.com>
2025-06-07 12:48:47 +0700

    update

--- app/core/App.php
+++ app/core/App.php
@@ -15,7 +15,16 @@ class App
             unset($url[0]);
         }
 
-        require_once '../app/controllers/' . $this->controller . '.php';
+        echo "Controller: " . $this->controller . "<br>";
+        echo "Looking for: " . __DIR__ . '/../controllers/' . $this->controller . '.php' . "<br>";
+
+        if (!file_exists(__DIR__ . '/../controllers/' . $this->controller . '.php')) {
+            die('File controller tidak ditemukan!');
+        }
+
+
+        require_once __DIR__ . '/../controllers/' . $this->controller . '.php';
+
         $this->controller = new $this->controller;
 
         if (isset($url[1]) && method_exists($this->controller, $url[1])) {
